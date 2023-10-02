7b515c14be1891b0c2e8d1dc0f1e146443a7ac30
IrinaMuravyova <miomir@yandex.ru>
2023-10-02 18:08:06 +0800

    App finished

--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.minimumValue = minimumValueLabel.text
+        settingsVC.maximumValue = maximumValueLabel.text
+        
+    }
+    
+    @IBAction func GetNumberButtonPressed() {
+        let minimumValue = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumValue = Int(maximumValueLabel.text ?? "") ?? 100
+        // minimumValue - не опциональное, а minimumValueLabel.text - опциональное
+        // поэтому сначала мы передаем пустую строку, если текст содержит nill
+        // потом это значение пытаемся преобразовать к Int
+        
+        randomNumberLabel.text = Int.random(in: minimumValue...maximumValue).formatted()
+    }
+    
+    @IBAction func saveButtonDidTapped(for segue: UIStoryboardSegue, sender: Any?){
+        guard let settingsVC = segue.source as? SettingsViewController else { return }
+        minimumValueLabel.text = settingsVC.minimumValueTF.text
+        maximumValueLabel.text = settingsVC.maximumValueTF.text
+    }
 }
 
